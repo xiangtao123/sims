d8133d6b1e8f3400a00170fc5c49d5a7ee5764cb
sunburst <xiang_tao123_happy@163.com>
2016-03-16 18:28:40 +0800

    dept page

---
+++ src/main/webapp/static/biz/sims/dept.js
@@ -0,0 +1,86 @@
+/**
+ * 部门管理
+ */
+var Dept = function(){};
+Dept.searchFormId = '#listForm';
+Dept.searchFormInputSelector = Dept.searchFormId + ' :input';
+Dept.searchParamKey  = null;
+Dept.dataGridId = "#dataGrid";
+Dept.init = function() {
+	var gridOptions = {
+		url: eui.basePath + '/dept/list',
+		method:'post',
+		toolbar: [
+		     { iconCls: 'icon-add', handler: function(){ Dept.saveOrUpdateDlg(false) }}
+		   , { iconCls: 'icon-edit', handler: function(){ Dept.saveOrUpdateDlg(true) }} , '-'
+		   , { iconCls: 'icon-remove', handler: function(){ Dept.remove() }}
+		],
+		columns:[[
+			{field:'id', title:'编号', width:80, checkbox:true },
+			{field:'deptName', title:'院系名称', width:120 },
+			{field:'type', title:'类型', width:100 },
+			{field:'remark', title:'备注', width:200 }
+		]]
+	};
+	eui.initGrid(Dept.dataGridId, gridOptions);
+};
+
+Dept.saveOrUpdateAjaxOptions = {
+	url 	: 	'/dept/saveOrUpdate',
+	success	:	function(data, st, xhr) {
+		if (data == 1) {
+			eui.alert('操作成功：信息成功保存');
+		} if (data == -1) {
+			eui.alert('操作失败：平台管理员不允许进行此操作','warning');
+		} else {
+			eui.alert('操作失败，程序运行异常','error');
+		}
+	}
+};
+
+
+Dept.bindEvent = function() {
+	$('#searchBtn').click(function(){
+		eui.loadDataGridWithFormDataSelector(Dept.dataGridId, Dept.searchFormInputSelector, Dept.searchParamKey);
+	});
+	
+	$('#clearBtn').click(function(){
+		$(Dept.searchFormId).form('clear');
+	});
+	
+	$('#saveOrUpdateBtn').click(function() {
+		var isValid = $(Dept.searchFormId).form('validate');
+		if (!isValid) {
+			return;
+		}
+		Dept.saveOrUpdateAjaxOptions.postData = eui.findFormDataJsonObject(Dept.searchFormInputSelector);
+		eui.commAjax(Dept.saveOrUpdateAjaxOptions);
+	});
+	
+	$('#cancelBtn').click(function(){
+		$('#saveOrUpdateDlg').dialog('close');
+	});
+	
+};
+
+$(function() {
+	Dept.init();
+	Dept.bindEvent();
+});
+
+
+Dept.saveOrUpdateDlg = function(isEdit) {
+	$('#saveOrUpdateForm').form('clear');
+	if (isEdit) {
+		var rows = $(Dept.dataGridId).datagrid('getChecked');
+		if (rows.length != 1) {
+			eui.alert('请选择一条数据进行编辑','warning');
+			return;
+		}
+		$('#saveOrUpdateForm').form('load', rows[0]);
+	}
+	$('#saveOrUpdateDlg').dialog('open');
+};
+
+
+
